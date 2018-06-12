df9fc654171fb517c515e8f08931fab3e6b754be
Ryan Clements <ryanclementsuf@gmail.com>
2018-06-12 19:26:52 -0400

    Made nav bar responsive

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent {}
+export class NavBarComponent {
+  private expanded = false;
+
+  toggleMenu() {
+    this.expanded = !this.expanded;
+  }
+}
