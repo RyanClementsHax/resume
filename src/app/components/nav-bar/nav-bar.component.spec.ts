fd825d3c449fe109457c37b1448974500c2766cb
Ryan Clements <ryanclementsuf@gmail.com>
2018-06-13 20:48:14 -0400

    Fixed tests and added dynamic dropdown sizing

--- src/app/components/nav-bar/nav-bar.component.spec.ts
+++ src/app/components/nav-bar/nav-bar.component.spec.ts
@@ -1,10 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavBarComponent } from './nav-bar.component';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('NavBarComponent', () => {
   let component: NavBarComponent;
   let fixture: ComponentFixture<NavBarComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +19,19 @@ describe('NavBarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NavBarComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('toggleMenu()', () => {
+    it('should toggle the expanded attribute', () => {
+      expect(component.expanded).toBe(false);
+      component.toggleMenu();
+      expect(component.expanded).toBe(true);
+    });
   });
 });
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent {
-  private expanded = false;
+export class NavBarComponent implements AfterViewInit {
+  @ViewChild('links') links: ElementRef;
+  expanded = false;
+  linksMaxHeight: number;
+
+  ngAfterViewInit() {
+    const linkHeight = this.links.nativeElement.childNodes[0].offsetHeight;
+    const linkCount = this.links.nativeElement.childNodes.length;
+    this.linksMaxHeight = linkCount * linkHeight;
+    console.log(this.links);
+  }
 
   toggleMenu() {
     this.expanded = !this.expanded;
