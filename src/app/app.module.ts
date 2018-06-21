8dfebf61709e48d2d44899bd0f3123486e51e6bd
Ryan Clements <ryanclementsuf@gmail.com>
2018-06-20 20:55:42 -0400

    Added parallax, changed color scheme, and added header text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ParallaxModule } from 'ngx-parallax';
+
 import { AppComponent } from './containers/app/app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HeaderJumbotronComponent } from './components/header-jumbotron/header-jumbotron.component';
@@ -12,7 +14,8 @@ import { HeaderJumbotronComponent } from './components/header-jumbotron/header-j
     HeaderJumbotronComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ParallaxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/header-jumbotron/header-jumbotron.component.ts
+++ src/app/components/header-jumbotron/header-jumbotron.component.ts
@@ -1,8 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { ParallaxConfig } from 'ngx-parallax';
 
 @Component({
   selector: 'header-jumbotron',
   templateUrl: './header-jumbotron.component.html',
   styleUrls: ['./header-jumbotron.component.scss']
 })
-export class HeaderJumbotronComponent {}
+export class HeaderJumbotronComponent {
+  private parallaxConfig: ParallaxConfig = {
+    ratio: -0.2
+  };
+}
