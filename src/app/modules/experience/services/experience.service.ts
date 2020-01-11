27a54a3116fddbb7dc048c717628c37197b268e7
Ryan Clements <ryanclementshax@gmail.com>
2020-01-10 20:19:51 -0500

    Update dependencies

--- src/app/modules/experience/services/experience.service.ts
+++ src/app/modules/experience/services/experience.service.ts
@@ -56,7 +56,7 @@ export class ExperienceService {
         startDate: new Date(2018, 5),
         endDate: new Date(2018, 8),
         description: `Ideated and implemented a design for an admin application
-            used to manage the complex data of a cross-domain menu structure using Stencil.js and Angular 5.`,
+         used to manage the complex data of a cross-domain menu structure using Stencil.js and Angular 5.`,
         technologies: ['Stencil.js', 'Angular 5', 'Web Components'],
         imgUrl: 'assets/img/ultimate-software-logo.jpg'
       },
@@ -67,8 +67,8 @@ export class ExperienceService {
         startDate: new Date(2017, 8),
         endDate: new Date(2017, 12),
         description: `Maintained a C# back end system called Transactor responsible for processing thousands of
-            customer energy usage data. Improved the company's internal tool used to manage customers and their data
-            by adding features that assisted customer representatives in delivering unparalleled customer service.`,
+         customer energy usage data. Improved the company's internal tool used to manage customers and their data
+         by adding features that assisted customer representatives in delivering unparalleled customer service.`,
         technologies: ['C#', 'ASP.NET'],
         imgUrl: 'assets/img/infinite-energy-logo.png'
       },
@@ -78,7 +78,7 @@ export class ExperienceService {
         position: 'Intern',
         startDate: new Date(2017, 5),
         endDate: new Date(2017, 8),
-        description: `I collaborated to develop an internal standup tool called UltiDash which integrated into Jira and the Aha! API's.`,
+        description: `Collaborated to develop an internal standup tool called UltiDash which integrated into Jira and the Aha! API's.`,
         technologies: ['C#', 'Angular', 'Web Api', 'Unity', 'Entity'],
         imgUrl: 'assets/img/ultimate-software-logo.jpg'
       },
@@ -89,7 +89,7 @@ export class ExperienceService {
         startDate: new Date(2016, 8),
         endDate: new Date(2017, 5),
         description: `Engineered the payment feature on the company website so the customers can make payments and
-            manage payment options.`,
+         manage payment options.`,
         technologies: ['Angular.js v1.6', 'C#', 'Web Api', 'Unity', 'Entity'],
         imgUrl: 'assets/img/infinite-energy-logo.png',
         linkUrls: [
@@ -105,8 +105,7 @@ export class ExperienceService {
         position: 'Intern',
         startDate: new Date(2016, 5),
         endDate: new Date(2016, 10),
-        description:
-          'I made a killer custom hybrid app with a fully functional back end and admin web app to manage it',
+        description: 'I made a killer custom hybrid app with a fully functional back end and admin web app to manage it.',
         technologies: [
           'Angular 1.6',
           'Cordova',
@@ -120,14 +119,12 @@ export class ExperienceService {
         imgUrl: 'assets/img/infinite-energy-industrial-ios.PNG',
         linkUrls: [
           {
-            url:
-              'https://play.google.com/store/apps/details?id=com.infiniteenergy.industrial.mobile&hl=en_US',
+            url: 'https://play.google.com/store/apps/details?id=com.infiniteenergy.industrial.mobile&hl=en_US',
             text: 'Google Play',
             disabledTitle: 'The company no longer wanted to support Android. :('
           },
           {
-            url:
-              'https://itunes.apple.com/us/app/infinite-energy-industrial/id1125474901?mt=8',
+            url: 'https://itunes.apple.com/us/app/infinite-energy-industrial/id1125474901?mt=8',
             text: 'App Store'
           }
         ]
